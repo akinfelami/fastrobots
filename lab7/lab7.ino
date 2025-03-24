14924eeb2d88d00255e6a11959cb86138562c696
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2025-03-24 11:29:09 -0400

    before fixing csv

--- lab7/lab7.ino
+++ lab7/lab7.ino
@@ -1,5 +1,5 @@
 #include "ICM_20948.h"
-#include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
+#include "SparkFun_VL53L1X.h"  //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
 #include <Wire.h>
 
 #include "BLECStringCharacteristic.h"
@@ -46,13 +46,16 @@ bool is_step_response = false;
 SFEVL53L1X distance_sensor_1(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
 SFEVL53L1X distance_sensor_2;
 
-enum command_types { PING, START_STEP, STOP_STEP, COLLECT_DATA };
+enum command_types { PING,
+                     START_STEP,
+                     STOP_STEP,
+                     COLLECT_DATA };
 
 EString tx_estring_value;
 RobotCommand robot_cmd(":|");
 
 void drive_in_a_straight_line(int direction, int pwm, float calib) {
-  if (direction) { // forward
+  if (direction) {  // forward
     analogWrite(0, pwm * calib);
     analogWrite(1, 0);
     analogWrite(2, 0);
@@ -105,47 +108,50 @@ void handle_command() {
   }
 
   switch (cmd_type) {
-  case PING:
-    tx_estring_value.clear();
-    tx_estring_value.append("PONG");
-    tx_characteristic_string.writeValue(tx_estring_value.c_str());
-
-    Serial.print("Sent back: ");
-    Serial.println(tx_estring_value.c_str());
-    break;
-  case COLLECT_DATA: {
-    size_t i;
-    while (i < curr_idx && i < MAX_DATA_SIZE) {
+    case PING:
       tx_estring_value.clear();
-      tx_estring_value.append(times[i]);
-      tx_estring_value.append("|");
-      tx_estring_value.append(pwm_vals[i]);
-      tx_estring_value.append("|");
-      tx_estring_value.append(tof_vals[i]);
-      tx_estring_value.append("|");
-      tx_estring_value.append(vels[i]);
+      tx_estring_value.append("PONG");
       tx_characteristic_string.writeValue(tx_estring_value.c_str());
-      i++;
-    }
-    // reset the index
-    curr_idx = 0;
-    break;
-  }
-  case START_STEP: {
-    is_step_response = true;
-    break;
-  }
-  case STOP_STEP: {
-    is_step_response = false;
-    distance_sensor_1.stopRanging();
-    distance_sensor_2.stopRanging();
-    stop();
-    curr_idx = 0;
-    break;
-  }
 
-  default:
-    break;
+      Serial.print("Sent back: ");
+      Serial.println(tx_estring_value.c_str());
+      break;
+    case COLLECT_DATA:
+      {
+        size_t i;
+        while (i < curr_idx && i < MAX_DATA_SIZE) {
+          tx_estring_value.clear();
+          tx_estring_value.append(times[i]);
+          tx_estring_value.append("|");
+          tx_estring_value.append(pwm_vals[i]);
+          tx_estring_value.append("|");
+          tx_estring_value.append(tof_vals[i]);
+          tx_estring_value.append("|");
+          tx_estring_value.append(vels[i]);
+          tx_characteristic_string.writeValue(tx_estring_value.c_str());
+          i++;
+        }
+        // reset the index
+        curr_idx = 0;
+        break;
+      }
+    case START_STEP:
+      {
+        is_step_response = true;
+        break;
+      }
+    case STOP_STEP:
+      {
+        is_step_response = false;
+        distance_sensor_1.stopRanging();
+        distance_sensor_2.stopRanging();
+        stop();
+        curr_idx = 0;
+        break;
+      }
+
+    default:
+      break;
   }
 }
 
@@ -153,40 +159,41 @@ void step_response(unsigned long test_duration, int step_pwm,
                    unsigned long start_time) {
   unsigned long current_time = start_time;
 
-  while ((current_time - start_time) < test_duration &&
-         curr_idx < MAX_DATA_SIZE) {
+  while ((current_time - start_time) < test_duration && curr_idx < MAX_DATA_SIZE) {
     // Apply step input
     drive_in_a_straight_line(0, step_pwm, 1.25);
 
     // Get sensor data
     distance_sensor_1.startRanging();
-    if (distance_sensor_1.checkForDataReady()) {
-      int current_distance = distance_sensor_1.getDistance();
-      distance_sensor_1.clearInterrupt();
-
-      // Get current timestamp
-      current_time = millis();
-
-      // Calculate velocity (change in position / change in time)
-      float velocity = 0;
-      if (curr_idx > 0) {
-        float delta_time =
-            (current_time - times[curr_idx - 1]) / 1000.0; // Convert to seconds
-        if (delta_time > 0) {
-          velocity = (current_distance - tof_vals[curr_idx - 1]) / delta_time;
-        }
-      }
-
-      // Record data
-      times[curr_idx] = current_time; // Record actual timestamp
-      pwm_vals[curr_idx] = step_pwm;
-      tof_vals[curr_idx] = current_distance;
-      vels[curr_idx] = velocity;
-      curr_idx++;
+    while (!distance_sensor_1.checkForDataReady()) {
+      delay(1);
     }
-    distance_sensor_1.stopRanging();
+
+    int current_distance = distance_sensor_1.getDistance();
+    distance_sensor_1.clearInterrupt();
+
+    // Get current timestamp
     current_time = millis();
+
+    // Calculate velocity (change in position / change in time)
+    float velocity = 0;
+    if (curr_idx > 0) {
+      float delta_time =
+        (current_time - times[curr_idx - 1]) / 1000.0;  // Convert to seconds
+      if (delta_time > 0) {
+        velocity = (current_distance - tof_vals[curr_idx - 1]) / delta_time;
+      }
+    }
+
+    // Record data
+    times[curr_idx] = current_time;  // Record actual timestamp
+    pwm_vals[curr_idx] = step_pwm;
+    tof_vals[curr_idx] = current_distance;
+    vels[curr_idx] = velocity;
+    curr_idx++;
   }
+  distance_sensor_1.stopRanging();
+  current_time = millis();
 }
 
 // void step_response() {
@@ -258,8 +265,8 @@ void setup() {
   Serial.begin(115200);
 
   // Left Wheels
-  pinMode(0, OUTPUT); // xIN2
-  pinMode(1, OUTPUT); // xIN1
+  pinMode(0, OUTPUT);  // xIN2
+  pinMode(1, OUTPUT);  // xIN1
 
   // Right Wheels
   pinMode(2, OUTPUT);
@@ -272,18 +279,18 @@ void setup() {
   distance_sensor_2.setI2CAddress(0x77);
   digitalWrite(SHUTDOWN_PIN, HIGH);
 
-  if (distance_sensor_1.begin() != 0) // Begin returns 0 on a good init
+  if (distance_sensor_1.begin() != 0)  // Begin returns 0 on a good init
   {
     Serial.println(
-        "Sensor 1 failed to begin. Please check wiring. Freezing...");
+      "Sensor 1 failed to begin. Please check wiring. Freezing...");
     while (1)
       ;
   }
 
-  if (distance_sensor_2.begin() != 0) // Begin returns 0 on a good init
+  if (distance_sensor_2.begin() != 0)  // Begin returns 0 on a good init
   {
     Serial.println(
-        "Sensor 2 failed to begin. Please check wiring. Freezing...");
+      "Sensor 2 failed to begin. Please check wiring. Freezing...");
     while (1)
       ;
   }
@@ -326,22 +333,26 @@ void loop() {
     if (rx_characteristic_string.written()) {
       handle_command();
     }
-    distance_sensor_1.startRanging();
     if (is_step_response) {
       // Record initial state
+      distance_sensor_1.startRanging();
+      while (!distance_sensor_1.checkForDataReady()) {
+        delay(1);
+      }
       unsigned long start_time = millis();
       if (curr_idx < MAX_DATA_SIZE) {
         times[curr_idx] = start_time;
-        pwm_vals[curr_idx] = 0; // Initial PWM is 0
+        pwm_vals[curr_idx] = 0;  // Initial PWM is 0
         tof_vals[curr_idx] =
-            distance_sensor_1.getDistance(); // Get current distance
-        vels[curr_idx] = 0;                  // Initial velocity is 0
+          distance_sensor_1.getDistance();  // Get current distance
+        vels[curr_idx] = 0;                 // Initial velocity is 0
         curr_idx++;
       }
+      distance_sensor_1.clearInterrupt();
       step_response(100, 0, start_time);
       unsigned long second_phase = millis();
 
-      step_response(3900, 100, second_phase);
+      step_response(5000, 100, second_phase);
       unsigned long third_phase = millis();
 
       step_response(1000, 0, third_phase);
